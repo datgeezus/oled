397d0d4cd1f4337e3a29ad1c2cf4d3612e014605
Jesus Sanchez <jesus.san.sev@gmail.com>
2014-02-03 10:06:11 -0600

    fixed drawPixel()

--- oled_draw.c
+++ oled_draw.c
@@ -3,5 +3,5 @@
 void oled_putPixel(uint8_t* buffer, uint8_t x, uint8_t y)
 {
 //    buffer[x + ((y / 8) * OLED_WIDTH)] = (1 << (y % 8));
-    buffer[x + ((y >> 3) * OLED_WIDTH)] = (1 << (y & 7));
+    buffer[x + ((y >> 3) * OLED_WIDTH)] |= (1 << (y & 7));
 }
